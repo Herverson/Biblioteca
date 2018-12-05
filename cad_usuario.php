31ff6e23933610fc7653229ed56ae9306034f311
marlo2222 <marloh2222@gmail.com>
2018-12-05 08:11:35 -0300

    mudanças

--- cad_usuario.php
+++ cad_usuario.php
@@ -9,31 +9,30 @@
 		<form id="form1" name="form1" method="post" action="salva_usuario.php">
 			<div class="form-group">
 				<label for="matricula">matricula</label>
-				<input type="text" class="form-control" id="matricula" placeholder="matricula">
+				<input type="text" class="form-control" id="matricula" name="matricula" placeholder="matricula">
 			</div>
 			<div class="form-group">
 				<label for="nome">Nome</label>
-				<input type="text" class="form-control" id="nome" placeholder="nome">
+				<input type="text" class="form-control" id="nome" name="nome" placeholder="nome">
 			</div>
 			<div class="form-group">
 				<label for="contato">Contato</label>
-				<input type="text" class="form-control" id="contato" placeholder="contato">
+				<input type="text" class="form-control" id="contato" name="contato" placeholder="contato">
 			</div>
 			<div class="form-group">
 				<label for="senha">Senha</label>
-				<input type="text" class="form-control" id="senha" placeholder="senha">
+				<input type="text" class="form-control" id="senha" name="senha" placeholder="senha">
+			</div>
+			<div class="form-group">
+				<label for="senha confirmacao">Senha confirmacao</label>
+				<input type="text" class="form-control" id="senha comfirmacao" name="senha_comfirmacao" placeholder="senha comfirmacao">
 			</div>
 			<!--<div class="form-group">
     			<label for="avatar">Avatar</label>
     			<input type="file" id="avatar">
     			<p class="help-block">Imagem do usuário</p>
 			</div>-->
-			<div class="form-group">
-				<label for="cpf">CPF</label>
-				<input type="text" class="form-control" id="cpf" placeholder="CPF">
-			</div>
-			<td><input type="hidden" name="acao" value="cadastra_usuario"></td>
-			<td><button type="submit" class="btn btn-primary">Cadastrar</button></td>
+			<input type="submit" value="Cadastrar" name="submit" class="btn btn-primary">
 		</form>
 	</div>
 <body>
--- salva_exemplar.php
+++ salva_exemplar.php
@@ -1,5 +1,6 @@
 <?php
 include('conexao.php');
+include('menu.php');
 $cod_livro = $_POST['cod_livro'];
 $ano = $_POST['ano'];
 $estado = $_POST['estado'];
@@ -7,11 +8,13 @@ $disponibilidade = $_POST['disponibilidade'];
 
 $query = "INSERT INTO exemplares (cod_livro, ano, estado, disponibilidade)  VALUES ('$cod_livro', '$ano', '$estado', '$disponibilidade')";
     mysqli_query($dbc, $query)
-      or die ();
+      or die ("<META HTTP-EQUIV=REFRESH CONTENT='0; URL=http://localhost/biblioteca/cad_exemplar.php'>
+	  <script type=\"text/javascript\">
+	  alert(\"Problema na solicitação\");
+	  </script>");
 	if($query)
 	{
-		echo"<META HTTP-EQUIV=REFRESH CONTENT='0; URL=http://localhost/biblioteca/listar_devolucao.html'>
-		<script type=\"text/javascript\">
+		echo"<META HTTP-EQUIV=REFRESH CONTENT='0; URL=http://localhost/biblioteca/cad_exemplar.html'><script type=\"text/javascript\">
 		alert(\"Cadastro realizado com sucesso.\");
 		</script>";
 	}
